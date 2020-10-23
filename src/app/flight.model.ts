4ed9924163c2dded6454ab01241fb7524c2e159f
Vladana Ignjatic <vladanaig@gmail.com>
2020-10-23 19:42:21 +0200

    First Commit

---
+++ src/app/flight.model.ts
@@ -0,0 +1,8 @@
+export interface Flight
+    { id?: number;
+     origin: string; 
+     destination: string; 
+     flightnumber: number; 
+     depart: Date; 
+     arrive: Date; 
+     nonstop: boolean }; 
